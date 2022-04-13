92a9ca7473607ed790363e1166873771d47fae03
kevinportella <kevinportella@gmail.com>
2022-04-13 14:30:03 -0300

    -add arq

---
+++ Estruturas_de_Controle/if.js
@@ -0,0 +1,30 @@
+function godNews(nota) {
+
+  if (nota >= 7) {  // se essa condição for cumprida, esse bloco vai ser executado
+
+    console.log('Approved: ' + nota)
+
+  }
+}
+
+godNews(8.1)
+godNews(6.1)
+
+function ifTrue(valor) {
+  if (valor) {
+    console.log(`It's true...`  + valor)
+  }
+}
+
+ifTrue()       // false
+ifTrue(null)   // false
+ifTrue(undefined) // false
+ifTrue(NaN)    // false
+ifTrue('')     // false
+ifTrue(0)      // false
+ifTrue(-1)     // true
+ifTrue(' ')    // true
+ifTrue('?')    // true
+ifTrue([])     // true
+ifTrue([1, 2]) // true
+ifTrue({})     // true
